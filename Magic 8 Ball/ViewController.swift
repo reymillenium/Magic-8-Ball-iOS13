e8df4f73547c64d02cebc41a87274d3096e332a8
Reinier Garcia Ramos <reymillenium@gmail.com>
2020-11-07 03:28:24 -0500

    Implemented onclick event on the Ask button, by randomly assigning an image to the bal8ImageViewer

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // Interface Builder Outlets (IB Outlets):
+    @IBOutlet weak var ball8ImageView: UIImageView!
+    
+    // Global constants:
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    // Interface Builder Actions (IB Actions):
+    @IBAction func buttonAsk(_ sender: UIButton) {
+        ball8ImageView.image = ballArray.randomElement()
+    }
+    
 }
 
